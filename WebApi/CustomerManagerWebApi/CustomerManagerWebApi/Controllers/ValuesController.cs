db2d267c5cd3512aeffc891efbf5b6de27de09cd
Yannick <yannick.jacques940@gmail.com>
2019-03-12 13:33:42 -0400

    #9-Ajouter les fichiers models et services + ajouter extensions mysql et dbcontext

--- WebApi/CustomerManagerWebApi/CustomerManagerWebApi/Controllers/ValuesController.cs
+++ WebApi/CustomerManagerWebApi/CustomerManagerWebApi/Controllers/ValuesController.cs
@@ -1,8 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace CustomerManagerWebApi.Controllers
 {
@@ -14,7 +11,7 @@ namespace CustomerManagerWebApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new[] { "value1", "value2" };
         }
 
         // GET api/values/5
