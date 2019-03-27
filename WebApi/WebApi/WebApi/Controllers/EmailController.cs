816eb682740c1b9629c16b6367b62717847a5308
Yannick <yannick.jacques940@gmail.com>
2019-03-27 10:59:12 -0400

    38-Ajouter l'email dans appsettings.json

---
+++ WebApi/WebApi/WebApi/Controllers/EmailController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using WebApi.Validators;
+
+namespace WebApi.Controllers
+{
+    public class EmailController : Controller
+    {
+        private readonly IConfiguration configuration;
+
+        public EmailController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpPost, Route("{message}")]
+        public void SendEmailToAdmins(string message)
+        {
+             EmailSender.SendEmail(message,configuration);
+        }
+    }
+}
