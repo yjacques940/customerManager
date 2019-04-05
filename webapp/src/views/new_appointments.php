42de85e172dcd5ac5feeedea2cd3e6821fc8a6e0
Yannick <yannick.jacques940@gmail.com>
2019-04-05 09:17:07 -0400

    Ajout sql + gérer les checkbox

--- webapp/src/views/new_appointments.php
+++ webapp/src/views/new_appointments.php
@@ -8,12 +8,51 @@ ob_start();
             for(var i=0, n=checkboxes.length;i<n;i++) {
                 checkboxes[i].checked = source.checked;
             }
+            changeButtonText();
+    }
+
+    function changeButtonText() {
+        hasToSave = hasACheckboxChecked();
+        button = document.getElementById('button_save_and_show_appointments');
+        button.firstChild.data = hasToSave ? "<?php echo localize('Button-ShowAppointmentsAndSave')?>":
+          "<?php echo localize('Button-ShowAppointments'); ?>"
+        unCheckTheCheckAllCheckbox(hasToSave);
+    }
+
+    function unCheckTheCheckAllCheckbox(hasToSave)
+    {
+        checkbox = document.getElementById('checkAllCheckboxes_id');
+        if(!hasToSave) { checkbox.checked = false; }
+        checkbox.checked = areAllChecked()
+    }
+
+    function areAllChecked()
+    {
+        checkboxes = document.getElementsByName('checkbox_new');
+        count =0;
+        for(var i=0, n=checkboxes.length;i<n;i++) {
+            if(checkboxes[i].checked)
+            {
+                count++;
+            }
+        }
+        return count == checkboxes.length ? true : false;
+    }
+    function hasACheckboxChecked()
+    {
+        checkboxes = document.getElementsByName('checkbox_new');
+        for(var i=0, n=checkboxes.length;i<n;i++) {
+            if(checkboxes[i].checked)
+            {
+                return true;
+            }
+        }
     }
 </script>
 <div class=" mx-auto" style="margin-top: 30px; width: 90%">
     <h3 class="title text-center mb-md-4 mb-sm-3 mb-3 mb-2"><?php echo localize('PageTitle-NewAppointments') ?></h3>
-        <a href="?action=appointments" class="btn btn-success" style="float:right; margin-bottom: 10px;">
-            <i class="fa fa-lg" ></i> <?php echo localize('Button-ShowAppointments'); ?>
+        <a href="?action=appointments" class="btn btn-success" style="float:right; margin-bottom: 10px;" id="button_save_and_show_appointments">
+            <?php echo localize('Button-ShowAppointments'); ?>
         </a>
 
     <table class="table table-sm table-striped table-hover table-bordered" id="tbl_appointments">
@@ -24,7 +63,7 @@ ob_start();
             <th scope="col"><?php echo localize('Appointment-Duration'); ?></th>
             <th scope="col"><?php echo localize('Appointment-Customer'); ?></th>
             <th scope="col"><?php echo localize('Appointment-ChangeNewStatus'); ?>
-                <input onClick="checkAll(this)" type="checkbox" ></th>
+                <input onClick="checkAll(this)" type="checkbox" id="checkAllCheckboxes_id"></th>
         </tr>
         </thead>
         <tbody>
@@ -66,7 +105,8 @@ ob_start();
                     ?>
                 </td>
                 <td class="align-middle text-center">
-                   <input id="<?php echo $appointment->appointment->id ?>" name="checkbox_new" type="checkbox" style="width:32px;height: 32px;">
+                   <input onClick="changeButtonText()" id="<?php echo $appointment->appointment->id ?>"
+                          name="checkbox_new" type="checkbox" style="width:32px;height: 32px;">
                 </td>
             </tr>
             <?php
