19d772f1a8c96662a426a9dba33049e6a578b5dd
Jessy Rodrigue <rodrigue_jes@hotmail.com>
2019-04-05 09:22:44 -0400

    Ajout des modeles des permissions pour les utilisateurs dans l'API

---
+++ webapi/WebApi/WebApi/Controllers/PermissionsController.cs
@@ -0,0 +1,12 @@
+﻿using WebApi.Models;
+using WebApi.Services;
+
+namespace WebApi.Controllers
+{
+    public class PermissionsController : BaseReaderController<PermissionService, Permission>
+    {
+        public PermissionsController(PermissionService service) : base(service)
+        {
+        }
+    }
+}
---
+++ webapi/WebApi/WebApi/Services/PermissionService.cs
@@ -0,0 +1,12 @@
+﻿using WebApi.Data;
+using WebApi.Models;
+
+namespace WebApi.Services
+{
+    public class PermissionService : BaseCrudService<Permission>
+    {
+        public PermissionService(IWebApiContext context) : base(context)
+        {
+        }
+    }
+}
