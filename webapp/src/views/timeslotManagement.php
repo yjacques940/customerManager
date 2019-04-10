d43c7b8ef0b4aae21a45fc3383679a22a4803b64
Jessy Rodrigue <rodrigue_jes@hotmail.com>
2019-04-10 16:08:17 -0400

    Ajout du bouton 'ajouter' et du get de la date et l'heure sélectionné

--- webapp/src/views/timeslotManagement.php
+++ webapp/src/views/timeslotManagement.php
@@ -15,14 +15,8 @@ ob_start(); ?>
 <script src='addons/fullcalendar-4.0.2/interaction/main.min.js'></script>
 
 <script>
-
-  var data = {
-      allDay: false,
-      startDate: "",
-      endDate: "",
-      startTime: "",
-      endTime: ""
-  };
+  var locale = '<?php echo $_SESSION['locale']; ?>';
+  var timeslot = null;
 
   document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
@@ -35,41 +29,65 @@ ob_start(); ?>
         nowIndicator: true,
         weekends: false,
         selectable: true,
+        unselectAuto: false,
         minTime: '8:00',
         maxTime: '22:00',
         select: function(info) {
-            var startDatetime = info.startStr.split('T');
-            var endDatetime = info.startStr.split('T');
+            var startDatetime = info.start;
+            var endDatetime = info.end;
+            if (info.allDay)
+                endDatetime.setDate(endDatetime.getDate() - 1);
             var start = {};
             var end = {};
-            start.date = startDatetime[0];
-            end.date = endDatetime[0];
-            if (!info.allDay) {
-                start.time = startDatetime[1].split('-')[0];
-                end.time = endDatetime[1].split('-')[0];
-            }
-            data = {
+            var dateOptions = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+            var timeOptions = {hour: '2-digit', minute: '2-digit'};
+            start.date = startDatetime.toLocaleDateString(locale + '-ca', dateOptions);
+            end.date = endDatetime.toLocaleDateString(locale + '-ca', dateOptions);
+            start.time = (info.allDay) ? null : ' ' + startDatetime.toLocaleTimeString('it-IT', timeOptions);
+            end.time = (info.allDay) ? null : ' ' + endDatetime.toLocaleTimeString('it-IT', timeOptions);
+            timeslot = {
                 allDay: (info.allDay),
                 startDate: start.date,
-                endDate: end.date,
                 startTime: start.time,
+                endDate: end.date,
                 endTime: end.time
             };
         },
+        unselect: function(info) {
+            timeslot = null;
+        },
         customButtons: {
             add_event: {
-                text: 'Add',
+                text: '<?php echo localize("Timeslot-Add") ?>',
                 click: function() {
-                    Swal.fire({
-                        title: data['allDay'].toString(),
-                        html: '<input type="datetime-local" min="<?php echo date('Y-m-d'); ?>" id="appointmentDate" class="form-control">',
-                        focusConfirm: false,
-                        preConfirm: () => {
-                            return [
-                                alert(document.getElementById('appointmentDate').value)
-                            ]
-                        }
-                    })
+                    var at = " <?php echo localize("Timeslot-At") ?> "
+                    var from = "<?php echo localize("Timeslot-From") ?> ";
+                    var le = "<?php echo localize("Timeslot-Le") ?> ";
+                    var to = "<?php echo localize("Timeslot-To") ?> ";
+                    if (timeslot !== null) {
+                        if (timeslot.startDate != timeslot.endDate)
+                            infoString = from + timeslot.startDate + at + timeslot.startTime
+                                + to + timeslot.endDate + at + timeslot.endTime;
+                        else
+                            if (timeslot.startTime === null)
+                                infoString = le + timeslot.startDate;
+                            else
+                                infoString = le + timeslot.startDate + ' '
+                                    + from + timeslot.startTime + at + timeslot.endTime;
+                        Swal.fire({
+                            title: "<strong>Nouvelle plage horaire</strong>",
+                            html: infoString +
+                                  '<input type="datetime-local" min="<?php echo date('Y-m-d'); ?>" id="appointmentDate" class="form-control">',
+                            focusConfirm: false,
+                            preConfirm: () => {
+                                return [
+                                    alert(document.getElementById('appointmentDate').value)
+                                ]
+                            }
+                        })
+                    } else {
+                        alert('No selection');
+                    }
                 }
             }
         },
