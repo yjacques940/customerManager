e92356755ffe04ef3e3ba61066558b75f5f94d7c
Yannick <yannick.jacques940@gmail.com>
2019-09-07 11:52:57 -0400

    223-Ajouter la base du projet

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,19 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { ToastrModule } from "ngx-toastr";
+
+import { SidebarModule } from './sidebar/sidebar.module';
+import { FooterModule } from './shared/footer/footer.module';
+import { NavbarModule} from './shared/navbar/navbar.module';
+import { FixedPluginModule} from './shared/fixedplugin/fixedplugin.module';
+
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { CustomerComponent } from './customer/customer.component';
+import { AppRoutes } from './app.routing';
+
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent
+    AdminLayoutComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(AppRoutes,{
+      useHash: true
+    }),
+    SidebarModule,
+    NavbarModule,
+    ToastrModule.forRoot(),
+    FooterModule,
+    FixedPluginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- web/src/app/customer/model/customer.ts
+++
@@ -1,9 +0,0 @@
-export class Customer {
-  id: number;
-  sex: string;
-  firstName: string;
-  lastName: string;
-  birthDate: Date;
-  occupation: string;
-  idAddress: number;
-}
